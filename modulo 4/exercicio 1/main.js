f2699bca884325ae4b567551e165eefa90c12703
HugoBrian <hugo_brian@hotmail.com>
2020-07-26 12:57:22 -0300

    Questâo 1 módulo 4

--- modulo 4/exercicio 1/main.js
+++ modulo 4/exercicio 1/main.js
@@ -1,5 +1,16 @@
 function checaIdade(idade) {
     // Retornar uma promise
+    
+        return new Promise(function(resolve, reject) {
+            setTimeout(function(){
+                if(idade >= 18){
+                    resolve();
+                }else {
+                    reject();
+                };
+            }, 2000)
+        });
+    
    }
 checaIdade(20)
     .then(function() {
@@ -8,3 +19,5 @@ checaIdade(20)
     .catch(function() {
         console.log("Menor que 18");
     });
+
+/*Usar funções anônimas function() quando é preciso passar um bloco como argumento de alguma função, para isso pode-se usar também functions arrows */
