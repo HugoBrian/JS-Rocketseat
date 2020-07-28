5adaaa74fd187062bad7dc23d23327279861f9f7
HugoBrian <hugo_brian@hotmail.com>
2020-07-28 15:46:11 -0300

    Aulas conceitos

--- JS ES6/Conceitos/main.js
+++ JS ES6/Conceitos/main.js
@@ -1,3 +1,118 @@
+//Aula Object Short Syntax
+/*
+const nome = 'Diego';
+const idade = 23;
+
+const usuario = {
+    nome,
+    idade,
+    empresa: 'Rocketseat',
+};
+
+console.log(usuario);
+*/
+
+//Aula Template Literals
+/*
+const nome = 'Diego';
+const idade = 23;
+
+console.log('Meu nome é '+nome+' e tenho '+idade+' anos')
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+*/
+
+// Aula rest e spread
+/*
+//REST
+
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    empresa: 'Rocketseat'
+};
+
+const { nome, ...resto} = usuario;
+
+console.log(nome);
+console.log(resto);
+
+const arr = [1, 2 ,3 , 4];
+
+const [a, b , ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(...params){
+    return params.reduce((total, next) => total + next);
+}
+
+console.log(soma(3,4,5));
+
+//SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+const usuario1 = {
+    nome: 'Diego',
+    idade: 23,
+    empresa: 'Rocketseat',
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel'};
+
+console.log(usuario2)
+*/
+
+// Aula Desestruturação
+/*
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+        cidade: 'Rio do Sul',
+        estado: 'SC',
+    },
+};
+
+const {nome , idade, endereco: {cidade}} = usuario;
+
+console.log(nome , idade, cidade);
+
+function mostraNome({nome}){
+    console.log(nome);
+}
+mostraNome(usuario)
+*/
+
+
+// Aula Valores Padrão
+/*
+const soma = (a = 0, b = 0) => a + b;
+
+console.log(soma(1));
+console.log(soma());
+*/
+
+
+// Aula Arrow Functions 
+/*
+const arr = [1, 3, 4, 5, 6];
+
+const newArr = arr.map(item => item * 2);
+
+console.log(newArr);
+
+const teste = () => ({ nome: 'Hugo'});
+
+console.log(teste());
+*/
 
 
 // Aula Operações em Array
