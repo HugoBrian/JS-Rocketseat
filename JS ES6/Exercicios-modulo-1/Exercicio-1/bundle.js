f3194f3f7e2b18f4fbd102b27b1fef86f726f0fe
HugoBrian <hugo_brian@hotmail.com>
2020-07-29 16:23:08 -0300

    Questão 2

--- JS ES6/Exercicios-modulo-1/Exercicio-1/bundle.js
+++ JS ES6/Exercicios-modulo-1/Exercicio-1/bundle.js
@@ -1,3 +1,34 @@
+// Questão 2
+
+/*
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+   ];
+
+
+const idades = usuarios.map(usuario => usuario.idade);
+
+console.log(idades);
+
+const maiores = usuarios.filter( usuario => usuario.idade>18 && usuario.empresa == 'Rocketseat');
+
+console.log(maiores);
+
+const google = usuarios.find(usuario => usuario.empresa == 'Google');
+
+console.log(google);
+
+let especiais = usuarios.map(function(usuario){
+    usuario.idade = usuario.idade * 2;
+    return usuario;
+});
+
+especiais = especiais.filter(usuario => usuario.idade<=50);
+
+console.log(especiais)
+*/
 // Questão 1
 
 /*
