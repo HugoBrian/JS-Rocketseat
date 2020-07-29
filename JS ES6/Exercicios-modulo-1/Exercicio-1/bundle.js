fe423a489e11392dc175847d31f374c2069b72b4
HugoBrian <hugo_brian@hotmail.com>
2020-07-29 16:56:01 -0300

    Questão 3

--- JS ES6/Exercicios-modulo-1/Exercicio-1/bundle.js
+++ JS ES6/Exercicios-modulo-1/Exercicio-1/bundle.js
@@ -1,4 +1,42 @@
-// Questão 2
+"use strict";
+
+// Questão 3
+//3.1
+var arr = [1, 2, 3, 4, 5];
+arr.map(function (item) {
+  return item + 10;
+}); //3.2
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23
+};
+
+var mostraIdade = function mostraIdade(aluno) {
+  return aluno.idade;
+}; //3.3
+
+
+var nome = "Diego";
+var idade = 23;
+
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); //3.4
+
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolved();
+  });
+}; // Questão 2
 
 /*
 const usuarios = [
@@ -58,4 +96,3 @@ const Adm1 = new Admin('email@teste.com', 'senha123');
 console.log(User1.isAdmin()) // false
 console.log(Adm1.isAdmin()) // true
  */
-"use strict";
--- JS ES6/Exercicios-modulo-1/Exercicio-1/main.js
+++ JS ES6/Exercicios-modulo-1/Exercicio-1/main.js
@@ -1,3 +1,24 @@
+// Questão 3
+
+//3.1
+const arr = [1, 2, 3, 4, 5];
+arr.map(item => item + 10);
+//3.2
+const usuario = {nome: 'Diego', idade: 23};
+const mostraIdade = aluno => aluno.idade;
+//3.3
+const nome = "Diego";
+const idade = 23;
+
+const mostraUsuario = (nome = 'Diego', idade = 18) => ({nome,idade}) ;
+
+console.log(mostraUsuario(nome,idade)); 
+console.log(mostraUsuario(nome));
+
+//3.4
+const promise = () => new Promise( (resolve,reject) => resolved())
+
+
 // Questão 2
 /*
 const usuarios = [
@@ -28,6 +49,13 @@ especiais = especiais.filter(usuario => usuario.idade<=50);
 
 console.log(especiais)
 */
+
+
+
+
+
+
+
 
 // Questão 1
 /*
