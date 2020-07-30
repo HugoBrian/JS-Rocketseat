48ecd2820e05ed2cbdcc51e4a34558be95c3b9cb
HugoBrian <hugo_brian@hotmail.com>
2020-07-30 19:12:51 -0300

    webpack export e import

---
+++ JS ES6/Webpack-Server/main.js
@@ -0,0 +1,8 @@
+import * as funcoes from './funcoes';
+//import somaFunction from './soma';
+
+console.log(funcoes);
+console.log(funcoes.mul(5,5));
+
+console.log(funcoes.soma(2,5672));
+console.log(funcoes.sub(2,2));
