d366de90bb949210f085141638fcc96e88a06bd8
HugoBrian <hugo_brian@hotmail.com>
2020-07-30 20:47:23 -0300

    Exercícios módulo 2

--- JS ES6/Webpack-Server/main.js
+++
@@ -1,8 +0,0 @@
-import * as funcoes from './funcoes';
-//import somaFunction from './soma';
-
-console.log(funcoes);
-console.log(funcoes.mul(5,5));
-
-console.log(funcoes.soma(2,5672));
-console.log(funcoes.sub(2,2));
---
+++ JS ES6/Webpack-Server/src/main.js
@@ -0,0 +1,10 @@
+import {Usuario as ClasseUsuario} from './funcoes';
+
+const usuario = new ClasseUsuario;
+
+console.log(usuario.info());
+
+
+import {idade} from './funcoes';
+
+console.log(idade);
