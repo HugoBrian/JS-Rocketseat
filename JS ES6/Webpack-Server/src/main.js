ebd48186fbe56723b93307930ecc0af8dfcf63d3
HugoBrian <hugo_brian@hotmail.com>
2020-07-31 15:58:29 -0300

    Aula Async/await

--- JS ES6/Webpack-Server/src/main.js
+++ JS ES6/Webpack-Server/src/main.js
@@ -1,3 +1,31 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("OK")}, 2000);
+});
+
+async function executaPromise () {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise();
+
+// Sem Async/await
+/*
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("OK")}, 2000);
+    setTimeout(() => { reject("not OK")}, 2100);
+});
+
+minhaPromise()
+.then(response => {
+    console.log(response);
+})
+.catch(err => {
+    console.log(err);
+});
+*/
+
 // Exercicios: Modulo 02
 /*
 import Usuario, {idade as IdadeUsuario} from './funcoes';
